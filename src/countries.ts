4406749e8782be27562be07458e87831d4e13189
mohsin ali <mohammadmohsin@gmail.com>
2016-05-06 21:38:27 +0500

    router configuration for countries

---
+++ src/countries.ts
@@ -0,0 +1,10 @@
+import { Component } from 'angular2/core';
+@Component({
+  selector: 'countries-list',
+  template: `
+    <hr><h3> Country List </h3>
+  `
+})
+export class Countries {
+    componentName: "Countries"
+}
--- src/dashboard.ts
+++ src/dashboard.ts
@@ -5,7 +5,6 @@ import { Component } from 'angular2/core';
   	<hr>
     <p>&nbsp;</p>
     <div class="container"><p align='center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p></div>
-    <p style="text-align:center;"><button>Browse Countires </button></p>
   `,
   styleUrls: ['src/styles.css']
 })
